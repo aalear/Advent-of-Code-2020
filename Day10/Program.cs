00e4bd614135577cf0b7a1e2e21fd1a41b92ce91
Adam Lear <aalear@users.noreply.github.com>
2020-12-11 23:03:01 -0500

    Day 10: Adapter Array (Part 1)

---
+++ Day10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var adapters = input.Select(i => int.Parse(i)).OrderBy(i => i).ToList();
+            adapters.Add(adapters.Last() + 3);
+
+            var diffCount = new Dictionary<int, int> {
+                { 1, 0 },
+                { 2, 0 },
+                { 3, 0}
+            };
+            diffCount[adapters[0]]++;
+
+            for(var i = 1; i < adapters.Count; i++)
+            {
+                var diff = adapters[i] - adapters[i - 1];
+                diffCount[diff]++;
+            }
+
+            Console.WriteLine("Part 1: " + (diffCount[1] * diffCount[3]));
+        }
+    }
+}
